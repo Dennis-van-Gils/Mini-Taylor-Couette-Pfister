e0cbff96af90ee0ff779e6541d6ae3172ee4c54a
Dennis van Gils <vangils.dennis@gmail.com>
2021-02-20 22:31:44 +0100

    Restore to original code format. Mark my changes with comment `DvG`

--- src_mcu/lib/Adafruit_Motor_Shield_V2_Library-1.0.11/Adafruit_MotorShield.h
+++ src_mcu/lib/Adafruit_Motor_Shield_V2_Library-1.0.11/Adafruit_MotorShield.h
@@ -23,7 +23,7 @@
 
 //#define MOTORDEBUG
 
-#define MICROSTEPS 8 // 8 or 16
+#define MICROSTEPS 8 // DvG: 8 or 16
 
 #define MOTOR1_A 2
 #define MOTOR1_B 3
@@ -47,68 +47,65 @@
 class Adafruit_MotorShield;
 
 /** Object that controls and keeps state for a single DC motor */
-class Adafruit_DCMotor
-{
+class Adafruit_DCMotor {
 public:
-    Adafruit_DCMotor(void);
-    friend class Adafruit_MotorShield; ///< Let MotorShield create DCMotors
-    void run(uint8_t);
-    void setSpeed(uint8_t);
+  Adafruit_DCMotor(void);
+  friend class Adafruit_MotorShield; ///< Let MotorShield create DCMotors
+  void run(uint8_t);
+  void setSpeed(uint8_t);
 
 private:
-    uint8_t PWMpin, IN1pin, IN2pin;
-    Adafruit_MotorShield *MC;
-    uint8_t motornum;
+  uint8_t PWMpin, IN1pin, IN2pin;
+  Adafruit_MotorShield *MC;
+  uint8_t motornum;
 };
 
 /** Object that controls and keeps state for a single stepper motor */
-class Adafruit_StepperMotor
-{
+class Adafruit_StepperMotor {
 public:
-    Adafruit_StepperMotor(void);
-    void setSpeed(uint16_t);
+  Adafruit_StepperMotor(void);
+  void setSpeed(uint16_t);
 
-    void step(uint16_t steps, uint8_t dir, uint8_t style = SINGLE);
-    uint8_t onestep(uint8_t dir, uint8_t style);
-    void release(void);
+  void step(uint16_t steps, uint8_t dir, uint8_t style = SINGLE);
+  uint8_t onestep(uint8_t dir, uint8_t style);
+  void release(void);
 
-    friend class Adafruit_MotorShield; ///< Let MotorShield create StepperMotors
-    uint8_t currentstep;               // DvG: Was private before
+  friend class Adafruit_MotorShield; ///< Let MotorShield create StepperMotors
+  uint8_t currentstep;               // DvG: Was private before
 
 private:
-    uint32_t usperstep;
+  uint32_t usperstep;
 
-    uint8_t PWMApin, AIN1pin, AIN2pin;
-    uint8_t PWMBpin, BIN1pin, BIN2pin;
-    uint16_t revsteps; // # steps per revolution
-    //uint8_t currentstep;  // DvG: We make it public to reset to 0
-    Adafruit_MotorShield *MC;
-    uint8_t steppernum;
+  uint8_t PWMApin, AIN1pin, AIN2pin;
+  uint8_t PWMBpin, BIN1pin, BIN2pin;
+  uint16_t revsteps; // # steps per revolution
+  //uint8_t currentstep;  // DvG: We make it public for debugging 
+  Adafruit_MotorShield *MC;
+  uint8_t steppernum;
 };
 
 /** Object that controls and keeps state for the whole motor shield.
     Use it to create DC and Stepper motor objects! */
-class Adafruit_MotorShield
-{
+class Adafruit_MotorShield {
 public:
-    Adafruit_MotorShield(uint8_t addr = 0x60);
+  Adafruit_MotorShield(uint8_t addr = 0x60);
 
-    void begin(uint16_t freq = 1600, TwoWire *theWire = NULL);
-    Adafruit_DCMotor *getMotor(uint8_t n);
-    Adafruit_StepperMotor *getStepper(uint16_t steps, uint8_t n);
+  void begin(uint16_t freq = 1600, TwoWire *theWire = NULL);
+  Adafruit_DCMotor *getMotor(uint8_t n);
+  Adafruit_StepperMotor *getStepper(uint16_t steps, uint8_t n);
 
-    friend class Adafruit_DCMotor; ///< Let DCMotors control the Shield
+  friend class Adafruit_DCMotor; ///< Let DCMotors control the Shield
 
-    void setPWM(uint8_t pin, uint16_t val);
-    void setPin(uint8_t pin, boolean val);
+  void setPWM(uint8_t pin, uint16_t val);
+  void setPin(uint8_t pin, boolean val);
 
 private:
-    TwoWire *_i2c;
-    uint8_t _addr;
-    uint16_t _freq;
-    Adafruit_DCMotor dcmotors[4];
-    Adafruit_StepperMotor steppers[2];
-    Adafruit_MS_PWMServoDriver _pwm;
+  TwoWire *_i2c;
+  uint8_t _addr;
+  uint16_t _freq;
+  Adafruit_DCMotor dcmotors[4];
+  Adafruit_StepperMotor steppers[2];
+  Adafruit_MS_PWMServoDriver _pwm;
 };
 
 #endif
